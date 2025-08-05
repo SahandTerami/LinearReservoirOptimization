ad00b6aed71f4c23b38d674d56612d4baa6b6245
Sahand Tangerami <sahanderami1995@gmail.com>
2025-08-05 22:26:23 +0330

    Delete Optimal Topology/optim.m

--- Optimal Topology/optim.m
+++
@@ -1,8 +0,0 @@
-function [params] = optim(K, N, beta1, beta2, gamma, a, b, phi, omega, c, kappa_init, lambda_init)
-
-        % Define parameters for Julia
-        params = [K, N, beta1, beta2, gamma, a, b, phi, omega, c, kappa_init, lambda_init];
-        
-        % Save to file to being read in Julia
-        writematrix(params, 'params.txt');
-end
